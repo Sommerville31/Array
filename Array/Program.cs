49c9011d9495603dc2e917e8466e86868ecf61a1
Sommerville31 <sebastiansommerville3@gmail.com>
2016-12-16 14:59:03 +0100

    Övning1

--- Array/Program.cs
+++ Array/Program.cs
@@ -10,6 +10,18 @@ namespace Array
     {
         static void Main(string[] args)
         {
+            //övning 1
+            string[] namn = new string[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Mata in 10 namn: ");
+                namn[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Dina vänner: ");
+            for (int i = 0; i < namn.Length; i++)
+            {
+                Console.Write(namn + ", ");
+            }
         }
     }
 }
